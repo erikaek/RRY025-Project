429e8cee498f1c53a2267806de3b46d43412fa3f
Erika Ek <erikaek@student.chalmers.se>
2017-09-27 13:03:48 +0200

    changes

--- Project4.m
+++ Project4.m
@@ -1,20 +1,21 @@
 clear all, clc, clf, close all;
 % frågor till alessandro: 
 % -vilket värde på A?
-% -zeropadding till filter storlek + bild, vad ska vi tänka oss som
-%  filter-storlek
-% -padda filtret?
-% -bara gL som gör skillnad?
-% -4 oberoende variabler, är gL och gH verkligen oberoende? Kan A räknas som en variabel? 
+% -är det bra att leta efter lokala skillnader? motsvarar det höga frekvenser i fourier?
+% -är det bra att vi "drar ut" bilden från 0 till 1? Blir f och g ens
+%   jämförbara då?
+% -vad händer med histogramet för skillnaden??
+% -det händer fortfarande bara grejer för gammaL?
+% -kvalitetsmått?
+% -det betyder det när "flippar" i var vi ändrar (stock eller buskage)?
 
 addpath(genpath('ProvidedFiles'))
 f=importdata('forest.mat');
 
-A=1;
-gammaL=[0.025 0.25 2.5];
-gammaH=2;
-kVec=1/(80^2);
-
+A=0;
+gammaL=0.025:0.1:2.5;
+gammaH=2;%[0.2 2 20];
+kVec=1/(80^2);%[0.01/(80^2) 0.1/(80^2) 1/(80^2)];
 
 n=1;
 for gL=gammaL
@@ -22,25 +23,29 @@ for gL=gammaL
         for k=kVec
             
             figure(n)
-            suptitle(['gL=',num2str(gL),' gH=',num2str(gH),' c/D0=',num2str(k)]);
-            %set(t,'interpreter','latex')
+            t=suptitle(['$\gamma_L$=',num2str(gL),' $\gamma_H$=',num2str(gH),' $c/D0$=',num2str(k)]);
+            set(t,'Interpreter','latex')
             subplot(3,2,1)
-            imshow(f,[])
+            %imshow((f-min(f(:)))/max(f(:)),[])
+            imagesc(f), colormap gray
             subplot(3,2,2)
-            imhist(f)
+            imhist((f-min(f(:)))/(max(f(:))-min(f(:))))
             
             g = homomorphic( f , A , gL, gH , k);
             
             subplot(3,2,3)
-            imshow(g,[])
+            %imshow((g-min(g(:)))/max(g(:)),[])
+            imagesc(g), colormap gray
             subplot(3,2,4)
-            imhist(g)
+            imhist((g-min(g(:)))/(max(g(:))-min(g(:))))
             
             subplot(3,2,5)
-            imshow(abs(f-g))
+            diff=(g-f);
+            %imshow((diff-min(diff(:)))/max(diff(:)),[])
+            imagesc(diff), colormap gray
             subplot(3,2,6)
-            imhist(abs(f-g))
-            
+            imhist((diff-min(diff(:)))/(max(diff(:))-min(diff(:))))
+            %imhist(diff)
             n=n+1;
             
         end
